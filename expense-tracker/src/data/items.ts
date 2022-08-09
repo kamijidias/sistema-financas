c6fee2b5608d4a8b1f4d41e6463fe6dcd41a14a5
Andrew Kamiji Dias <andrew_kamiji_dias@hotmail.com>
2022-08-09 12:49:59 -0300

    Começando o sistema de finanças

---
+++ expense-tracker/src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item'
+
+export const items: Item[] =[
+    { date: new Date(2022, 9, 9), category: 'food', title: 'McDonalds', value: 32.12 },
+    { date: new Date(2022, 9, 9), category: 'food', title: 'Burguer King', value: 28 },
+    { date: new Date(2022, 9, 12), category: 'rent', title: 'Aluguel APT', value: 2000 },
+    { date: new Date(2022, 10, 18), category: 'salary', title: 'Salário ACME', value: 4000 }
+];
